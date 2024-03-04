7bdb7fae13903739bcdb712174ff21bc58a98c17
moustafa elsayed <moelsayed949@gmail.com>
2024-03-04 21:38:45 +0200

    add items to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "../../images/logo.png";
 import Image from "next/image";
 import { SlLocationPin } from "react-icons/sl";
+import { IoIosSearch } from "react-icons/io";
+import { BiCaretDown } from "react-icons/bi";
 
 const Header = () => {
   return (
@@ -18,13 +20,30 @@ const Header = () => {
         {/* Dilvery */}
         <div className="px-2 border border-transparent hover:border-white  items-center justify-center h-[70%] duration-300 cursor-pointer hidden xl:inline-flex gap-1">
           <SlLocationPin className="text-white" />
-          <div className="text-xs">
+          <div className="text-sm">
             <p className="text-white">Dilver to</p>
             <p className="text-white font-bold uppercase">usa</p>
           </div>
         </div>
         {/* SearchBar */}
+        <div className="flex-1 h-10 hidden md:inline-flex items-center justify-between relative ">
+          <input
+            type="text"
+            placeholder="Search next_amazon_yt products"
+            className="w-full h-full rounded-md px-2 outline-none placeholder:tex-sm text-base text-black border-transparent border-[3px] focus-visible:border-amazon_yellow"
+          />
+          <span className="w-12 h-full rounded-tr-md rounded-br-md bg-amazon_yellow text-black text-2xl flex items-center justify-center absolute right-0">
+            <IoIosSearch />
+          </span>
+        </div>
         {/* sign in */}
+        <div className=" text-white text-xs flex-col px-2 border border-transparent hover:border-white  items-center justify-center h-[70%] duration-300 cursor-pointer hidden xl:inline-flex gap-1">
+          <p>Hello,sign in</p>
+          <p className="flex justify-between items-center font-bold">
+            Account,lists
+            <BiCaretDown />
+            </p>
+        </div>
         {/* favorite  */}
         {/* cart */}
       </div>
