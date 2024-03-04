ea72aa00a40be66be955990f20634f7e734d7086
moustafa elsayed <moelsayed949@gmail.com>
2024-03-05 00:53:06 +0200

    add bottomHeader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="font-bodyFont">
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "@/components/header/Header";
+import BottomHeader from "@/components/header/BottomHeader";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main >
+    <main>
       <Header />
+      <BottomHeader />
     </main>
   );
 }
