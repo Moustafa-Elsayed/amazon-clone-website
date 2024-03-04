efac94150554d638a2ebc96436ed49a575fb8b71
moustafa elsayed <moelsayed949@gmail.com>
2024-03-04 20:49:18 +0200

    add component of header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,8 +3,16 @@ import logo from "../../images/logo.png";
 import Image from "next/image";
 const Header = () => {
   return (
-    <div>
-      <Image src={logo} alt="logoImage" />
+    <div className="w-full h-20 bg-amazon_blue sticky top-0 z-50">
+      <div className="w-full h-full mx-auto inline-flex justify-between items-center gap-1 mdl:gap-4 px-4">
+        {/* logo */}
+        <Image className="w-28 object-cover" src={logo} alt="logoImage" />
+        {/* Dilvery */}
+        {/* SearchBar */}
+        {/* sign in */}
+        {/* favorite  */}
+        {/* cart */}
+      </div>
     </div>
   );
 };
