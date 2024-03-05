3120ffec527e9ca2bc1b2969a4f784b6873b8ffa
moustafa elsayed <moelsayed949@gmail.com>
2024-03-06 01:22:20 +0200

    Fetch data from API (SSR)

--- src/components/header/BottomHeader.tsx
+++ src/components/header/BottomHeader.tsx
@@ -4,26 +4,26 @@ import { MdOutlineMenu } from "react-icons/md";
 const BottomHeader = () => {
   return (
     <div className="bg-amazon_light h-10 w-full text-white text-sm px-4 flex items-center ">
-      <p className="flex  items-center px-2 gap-1 h-8 border border-transparent hover:border-white duration-300">
+      <p className="cursor-pointer flex  items-center px-2 gap-1 h-8 border border-transparent hover:border-white duration-300">
         <MdOutlineMenu />
         ALL
       </p>
-      <p className=" hidden md:inline-flex items-center px-2 gap-1 h-8 border border-transparent hover:border-white duration-300">
+      <p className="  hidden cursor-pointer md:inline-flex items-center px-2 gap-1 h-8 border border-transparent hover:border-white duration-300">
         Todays Deals
       </p>
-      <p className=" hidden md:inline-flex items-center px-2 gap-1 h-8 border border-transparent hover:border-white duration-300">
+      <p className="  hidden cursor-pointer md:inline-flex items-center px-2 gap-1 h-8 border border-transparent hover:border-white duration-300">
         Customer Service
       </p>
-      <p className=" hidden md:inline-flex items-center px-2 gap-1 h-8 border border-transparent hover:border-white duration-300">
+      <p className="  hidden cursor-pointer md:inline-flex items-center px-2 gap-1 h-8 border border-transparent hover:border-white duration-300">
         Registry
       </p>
-      <p className=" hidden md:inline-flex items-center px-2 gap-1 h-8 border border-transparent hover:border-white duration-300">
+      <p className="  hidden cursor-pointer md:inline-flex items-center px-2 gap-1 h-8 border border-transparent hover:border-white duration-300">
         Gift Cards
       </p>
-      <p className=" hidden md:inline-flex items-center px-2 gap-1 h-8 border border-transparent hover:border-white duration-300">
+      <p className="  hidden cursor-pointer md:inline-flex items-center px-2 gap-1 h-8 border border-transparent hover:border-white duration-300">
         Sell
       </p>
-      <p className=" hidden md:inline-flex text-amazon_yellow items-center px-2 gap-1 h-8 border border-transparent hover:border-amazon_yellow duration-300">
+      <p className="  hidden cursor-pointer md:inline-flex text-amazon_yellow items-center px-2 gap-1 h-8 border border-transparent hover:border-amazon_yellow duration-300">
         Sign Out
       </p>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
 import Banner from "@/components/Banner";
+import Products from "@/components/Products";
+import { log } from "console";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({productData}) {
+  console.log(productData);
+  
   return (
     <main>
       <div className="max-w-screen mx-auto">
         <Banner />
+        <Products />
       </div>
     </main>
   );
 }
+//SSR for data fetching
+export const getServerSideProps = async () => {
+  const res = await fetch("https://fakestoreapiserver.reactbd.com/tech");
+  const productData = await res.json();
+  return { props: { productData } };
+};
