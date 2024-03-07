969c1e2881f3839694f4dcb03166d2fa169bc71a
moustafa elsayed <moelsayed949@gmail.com>
2024-03-07 22:52:11 +0200

    add redux with persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
 import nextReducer from "./nextSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, nextReducer);
+
 export const store = configureStore({
-  reducer: {
-    next: nextReducer,
-  },
+  reducer: { next: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export let persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
