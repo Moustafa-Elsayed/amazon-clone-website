23d6fed986377407f3cfb22917e323e93a37deb5
moustafa elsayed <moelsayed949@gmail.com>
2024-03-10 02:04:29 +0200

    add meta form pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,19 @@ import type { AppProps } from "next/app";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Amazon clone</title>
+        <meta
+          name="description"
+          content="Super product with free shipping."
+          key="desc"
+        />
+      </Head>
       <div className="font-bodyFont bg-gray-300">
         <RootLayout>
           <Component {...pageProps} />
--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,7 +1,20 @@
+import Head from "next/head";
 import React from "react";
 
 const Cart = () => {
-  return <div className="text-orange">Cart</div>;
+  return (
+    <>
+      <Head>
+        <title>Cart</title>
+        <meta
+          name="description"
+          content="Super product with free shipping."
+          key="desc"
+        />
+      </Head>
+      <div className="text-orange">Cart</div>;
+    </>
+  );
 };
 
 export default Cart;
