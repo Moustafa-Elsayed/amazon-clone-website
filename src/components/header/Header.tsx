833d1e6313c4584954bdf900c6b859e2bebdfb7c
moustafa elsayed <moelsayed949@gmail.com>
2024-03-14 14:05:55 +0200

    add item

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-"use client";
 import React, { useEffect, useState } from "react";
 import logo from "../../images/logo.png";
 import Image from "next/image";
