cb6fd4dcea00b7c04a25394742a8daec1047ec59
moustafa elsayed <moelsayed949@gmail.com>
2024-03-15 14:26:54 +0200

    add favorite page

--- src/pages/favorite.tsx
+++ src/pages/favorite.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { StoreProduct, productProps, stateProps } from "../../type";
+import { useSelector } from "react-redux";
+import Image from "next/image";
+import FavoriteProduct from "@/components/FavoriteProduct";
 
-const favorite = () => {
+const Favorite = () => {
+  const { productData, favoriteData } = useSelector(
+    (state: stateProps) => state.next
+  );
+
   return (
-    <div>favorite</div>
-  )
-}
+    <>
+      {favoriteData.length > 0 ? (
+        <div className="max-w-screen-2xl mx-auto px-6 grid grid-cols-2 gap-10 py-4">
+          <div className="bg-white col-span-4 p-4 rounded-lg ">
+            {favoriteData.map((item: StoreProduct) => (
+              <div key={item._id}>
+                <FavoriteProduct item={item} />
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <h1>empty</h1>
+      )}
+    </>
+  );
+};
 
-export default favorite
+export default Favorite;
