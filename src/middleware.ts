1cdaa6ce250e9faf3620b6acaaaef35ec3d562e8
moustafa elsayed <moelsayed949@gmail.com>
2024-03-17 00:34:02 +0200

    fixed issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  ignoredRoutes: ["/", "/favorite","/cart" ],
+  ignoredRoutes: ["/", "/favorite", "/cart", "/[_id]"],
 });
 
 export const config = {
